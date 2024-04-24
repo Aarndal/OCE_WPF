02476917402fe9e2052b14759d3c40f611cde89f
DESKTOP-UFEEQ7V\pasca <Pascal.keller12@gmail.com>
2024-04-24 15:21:53 +0200

    Keine ahnung guck code

---
+++ OCE_WPF/Card.cs
@@ -0,0 +1,37 @@
+﻿namespace OCE_WPF
+{
+
+    public enum Priority
+    {
+        NotImportant,
+        ABitImportant,
+        MoreImportant,
+        KindaImportant,
+        JoaSchonWichtig
+    }
+
+    internal class Card
+    {
+        public int Id;
+        public string title { get; set; }
+        public string description { get; set; }
+        public Tuple<int, int, int> date { get; set; }
+        public Priority priority { get; set; }
+        public List<string> category { get; set; }
+        public bool isDone { get; set; }
+
+        public Card()
+        {
+            title = "Title";
+            description = "Description";
+            date = Tuple.Create(0, 0, 0);
+            priority = Priority.MoreImportant;
+            category = new List<string>();
+            category.Add("Hamburger");
+            category.Add("Cheeseburger");
+            category.Add("Other");
+            isDone = false;
+        }
+
+    }
+}
---
+++ OCE_WPF/CardList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCE_WPF
+{
+    internal class CardList : ObservableCollection<Card>
+    {
+        public List<Card> taskCards { get; set; }
+        public CardList() 
+        { 
+            taskCards = new List<Card>();
+        }   
+        public void CreateCard()
+        {
+            taskCards.Add(new Card());
+        }
+
+        public List<Card> GetActiveCards()
+        {
+            List<Card> resultList = new List<Card>();
+            foreach(Card card in taskCards)
+            {
+                if(!card.isDone)
+                {
+                    resultList.Add(card);
+                }
+            }
+            return resultList;
+        }
+        public List<Card> GetFinishedCards()
+        {
+            List<Card> resultList = new List<Card>();
+            foreach (Card card in taskCards)
+            {
+                if (card.isDone)
+                {
+                    resultList.Add(card);
+                }
+            }
+            return resultList;
+        }
+    }
+}
