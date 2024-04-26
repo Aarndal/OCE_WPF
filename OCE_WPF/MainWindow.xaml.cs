654d25b854be796fa17c5e75b3bd530a2046b5bc
Ratzeeh [DP] <dennis.poltorazki@gmail.com>
2024-04-26 12:22:37 +0200

    option menu added

--- OCE_WPF/MainWindow.xaml.cs
+++ OCE_WPF/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace OCE_WPF
         {
             InitializeComponent();            
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
